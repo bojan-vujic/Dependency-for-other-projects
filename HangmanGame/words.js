da2098d4f55adc1b5418988ebd7cdc2f43ca51f6
bojan-vujic <bojan.vujic@yahoo.com>
2022-05-20 13:49:09 +0200

    Create words.js

---
+++ HangmanGame/words.js
@@ -0,0 +1,133 @@
+var words =[
+  'you are a litle boy dady',
+  'Happy little girl that is you',
+  'you are a happy dady bikoz you are my dady',
+  'I love you dady because you are my dady huw are happy',
+]
+
+var words = [
+    'Tajra je najslađi ćuko na svijetu',
+    'tortilja mmmm',
+    'Obožavam sladoled i čokoladu',
+    'Maslačak',
+    'Ljubičica',
+    'Banana i ananas',
+    'Moj švrćan mali voli te tvoj tajo',
+    'Čoklada i sladoled su njamis njam',
+    'Suncokret je lijep',
+    'Volimo te i ti si najslađa mama na cijelom svijetu',
+    'Stolica',
+    'Cvijeće lijepo miriše',
+    'Visibaba',
+    'Volim da gledam crtane filmove',
+    'Med je slađi od šećera',
+    'Sat kuca tika taka',
+    'Slikarsko platno',]
+
+var rijeci = {
+  'voće_ili_povrće' :
+    [
+      'Banana',
+      'Kruška',
+      'Jabuka',
+      'Dunja',
+      'Višnja',
+      'Grožđe',
+      'Breskva',
+      'Šljiva',
+      'Patlidžan',
+      'Salata',
+      'Kupus',
+      'Mrkva',
+      'Šargarepa',
+      'Lubenica',
+      'Dinja',
+      'Paradajz',
+      'Krastavac',
+      'Krompir'
+    ],
+  
+  'predmet' :
+    [
+      'Krevet',
+      'Stolica',
+      'Šolja',
+      'Monitor',
+      'Laptop',
+      'Pernica',
+      'Olovka',
+      'Mašinica',
+      'Torba',
+      'Tanjir',
+      'Viljuška',
+      'Kašika',
+      'Radiator',
+      'Daljinski',
+      'Televizor',
+      'Ogledalo',
+      'Komoda',
+      'Vaza',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+      '',
+    ],
+
+  'muzički_instrument' :
+    [
+      'Piano',
+      'Gitara',
+      'Bobnjevi',
+      'Violina',
+      'Flauta',
+      '',
+      '',
+      '',
+      '',
+      '',
+    ]
+  
+}
+
+var frequencyJug = {
+  'a' : 10,
+  'o' : 14,
+  'i' : 16,
+  'e' : 18,
+  'n' : 27,
+  's' : 28,
+  't' : 29,
+  'r' : 30,
+  'u' : 31,
+  'v' : 34,
+  'm' : 34,
+  'd' : 34,
+  'k' : 34,
+  'p' : 36,
+  'j' : 37,
+  'l' : 37,
+  'z' : 40,
+  'g' : 40,
+  'b' : 42,
+  'č' : 42,
+  'nj': 42,
+  'š' : 42,
+  'c' : 43,
+  'h' : 43,
+  'ž' : 43,
+  'lj': 43,
+  'ć' : 43,
+  'f' : 44,
+  'đ' : 44,
+  'đž': 45
+}
+
+
+
+
+
+export { rijeci, frequencyJug };
+
